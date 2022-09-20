f5490872515567e64dab6bcf5bf16e66458c173e
Nikolay Vlaskin <Nikolay.Vlaskin@gmail.com>
2022-09-21 01:05:07 +0700

    Написали программу, решающую поставленную задачу

---
+++ FinishHomework/Program.cs
@@ -0,0 +1,38 @@
+﻿// ---------------------------------------------------------------
+/*
+Условия задачи:
+Написать программу, которая из массива строк формирует массив из строк, 
+длина которых меньше либо равна 3 символа. 
+
+Пример:
+[hello; 2; world; :-)] -> [2; :-)]
+
+*/
+// ---------------------------------------------------------------
+
+Console.Write("Введите через пробел массив строк: ");
+string [] array = Console.ReadLine().Split(); 
+int size = array.Length;
+string [] arraytemp = new string [size];
+int k = 0;
+
+for (int i = 0; i < size; i++)
+{
+    if (array[i].Length <= 3) 
+    {   
+        arraytemp[k] = array[i];
+        k++;
+    }
+
+}
+string [] arrayresult = new string [k];
+
+for (int i = 0; i < k; i++)
+{
+    arrayresult[i] = arraytemp[i];
+}
+
+Console.WriteLine();
+Console.WriteLine($"Первоначальный массив строк: [ {String.Join("; ", array)} ]");
+Console.WriteLine($"Результирующий массив строк: [ {String.Join("; ", arrayresult)} ]");
+
